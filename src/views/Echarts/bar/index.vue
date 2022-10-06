b5227582265a7e7bf81792bf4278d6464ebd79a4
hboot <bobolity@163.com>
2022-10-06 15:34:15 +0800

    echarts 依赖

---
+++ src/views/Echarts/bar/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bar-chart">
+    <el-row>
+      <el-col :span="12">
+        <div ref="chart" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  setup() {},
+}
+</script>
+<style lang="less" scoped>
+  .bar-chart {
+  }
+</style>
